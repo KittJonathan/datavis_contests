5fc3ad4bd7e6381c8a17264bfa27b7ff872c156d
Jonathan Kitt <jonathan.kitt@inrae.fr>
2023-09-26 12:04:57 +0200

    JSS contest ok

--- 2023_09_27_john_snow_society/2023_09_27_john_snow_cholera_map.R
+++ 2023_09_27_john_snow_society/2023_09_27_john_snow_cholera_map.R
@@ -2,19 +2,22 @@
 ## John Snow Society 210th Anniversary Cholera Map Competition
 ## https://johnsnowsociety.org/broadsheets-lectures-social/#mapcompetition
 ## Deadline 2023-09-27
-## Last update 2023-09-22
+## Last update 2023-09-26
 
-## Load packages ----
+## 📦 Load the packages ----
 
 library(tidyverse)
 library(HistData)
+library(showtext)
 
-## Import the datasets ----
+## 🔤 Import fonts ----
 
-# broad_st_pump <- HistData::Snow.pumps |>
-#   as_tibble() |>
-#   filter(label == "Broad St") |>
-#   select(x, y)
+font_add_google("Roboto Condensed", "Roboto Condensed")
+font_add_google("Raleway", "Raleway")
+font_add_google("Raleway Dots", "Raleway Dots")
+showtext_auto()
+
+## ⬇️ Import the datasets ----
 
 deaths <- HistData::Snow.deaths |>
   as_tibble() |>
@@ -24,61 +27,53 @@ pumps <- HistData::Snow.pumps |>
   as_tibble() |>
   rename(x_pump = x, y_pump = y)
 
+## 🧹 Clean & prep the data ----
+
 pumps_rep <- bind_rows(replicate(nrow(deaths),
-                                 pumps, simplify = FALSE)) |>
+                                 pumps,
+                                 simplify = FALSE)) |>
   mutate(case = rep(deaths$case, each = nrow(pumps)),
          .before = pump)
 
 cases_pumps <- pumps_rep |>
   left_join(deaths) |>
-  mutate(dist = sqrt((x_case - x_pump)^2 + (y_case - y_pump)^2))
-
-min_dist <- cases_pumps |>
+  mutate(dist = sqrt((x_case - x_pump)^2 + (y_case - y_pump)^2)) |>
   slice_min(dist, by = case)
 
+rm(deaths, pumps, pumps_rep)
+
+## ✏️ Create the plot ----
+
 p <- ggplot() +
-  geom_point(data = filter(min_dist, !label %in% c("Broad St", "So Soho", "Crown Chapel",
-                                                   "Briddle St")),
-             aes(x = x_case, y = y_case),
-             col = "grey90", size = 0.2) +
-  geom_point(data = filter(min_dist, label == "Broad St"),
-             aes(x = x_case, y = y_case),
-             col = "cyan4", size = 0.2) +
-  geom_point(data = filter(min_dist, label == "So Soho"),
-             aes(x = x_case, y = y_case),
-             col = "darkorange", size = 0.2) +
-  geom_point(data = filter(min_dist, label == "Crown Chapel"),
-             aes(x = x_case, y = y_case),
-             col = "purple", size = 0.2) +
-  geom_point(data = filter(min_dist, label == "Briddle St"),
-             aes(x = x_case, y = y_case),
-             col = "gold2", size = 0.2) +
-  geom_segment(data = filter(min_dist, label == "Broad St"),
+  geom_segment(data = filter(cases_pumps, label == "Broad St"),
                aes(x = x_case, xend = x_pump,
                    y = y_case, yend = y_pump),
-               col = "cyan4", linewidth = 0.1) +
-  geom_segment(data = filter(min_dist, label == "So Soho"),
-               aes(x = x_case, xend = x_pump,
-                   y = y_case, yend = y_pump),
-               col = "darkorange", linewidth = 0.1) +
-  geom_segment(data = filter(min_dist, label == "Crown Chapel"),
-               aes(x = x_case, xend = x_pump,
-                   y = y_case, yend = y_pump),
-               col = "purple", linewidth = 0.1) +
-  geom_segment(data = filter(min_dist, label == "Briddle St"),
-               aes(x = x_case, xend = x_pump,
-                   y = y_case, yend = y_pump),
-               col = "gold2", linewidth = 0.1)
+               colour = "#02f0fc", linewidth = 0.2) +
+  geom_point(data = cases_pumps,
+             aes(x = x_case, y = y_case),
+             shape = 21, size = 2,
+             colour = "#03a4da", fill = "#040940") +
+  geom_point(data = filter(cases_pumps, label == "Broad St"),
+             aes(x = x_case, y = y_case),
+             shape = 21, size = 2,
+             colour = "#02f0fc", fill = "#040940") +
+  labs(title = "Map of the 1854 cholera outbreak",
+       subtitle = "Each dot represents a case and the lines connect the cases closest to the Broad Street pump",
+       caption = "John Snow Society 210th Anniversary Cholera Map Competition | Visualisation by Jonathan Kitt") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "#040940", colour = "#040940"),
+        plot.background = element_rect(fill = "#040940", colour = "#040940"),
+        plot.title = element_text(family = "Raleway", colour = "#02f0fc",
+                                  face = "bold", size = 50, hjust = 0.5,
+                                  margin = margin(t = 20)),
+        plot.subtitle = element_text(family = "Raleway", colour = "#02f0fc",
+                                     size = 40, hjust = 0.5,
+                                     margin = margin(t = 5)),
+        plot.caption = element_text(family = "Raleway", colour = "#02f0fc",
+                                    size = 20, hjust = 0.5,
+                                    margin = margin(b = 5)))
+
+## 💾 Export plot ----
 
 ggsave("2023_09_27_john_snow_society/2023_09_27_john_snow_cholera_map.png",
        p, dpi = 320, width = 12, height = 6)
-
-
-
-  geom_point(aes(x = x_case, y = y_case, col = label)) +
-  geom_segment(aes(x = x_case, xend = x_pump,
-                   y = y_case, yend = y_pump,
-                   col = label),
-               linewidth = 0.01)
-
-ggsave("2023_09_27_john_snow_society/2023_09_27_john_snow_cholera_map.png", p, dpi = 320, width = 12, height = 6)
